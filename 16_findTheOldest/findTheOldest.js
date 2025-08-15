f708bbe32fc18558d0c0c8f2b0fa3fca8b2d73c1
Ramon <217152112+OrkByte@users.noreply.github.com>
2025-08-15 21:29:35 +0700

    Solve Exercises
    Solve exercise 12 to 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(persons) {
+  let oldestPerson = null;
+  let oldestAge = 0;
 
+  const yearToday = new Date().getFullYear();
+
+  persons.forEach(person => {
+    const personsAge = (person.yearOfDeath ?? yearToday) - person.yearOfBirth;
+    
+    if (personsAge > oldestAge) {
+      oldestAge = personsAge;
+      oldestPerson = person;
+    }
+  });
+
+  return oldestPerson;
 };
 
 // Do not edit below this line
