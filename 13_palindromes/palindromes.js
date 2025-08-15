b548e3cf2f33b2539e773d8a8f7052fe51a76e08
Ramon <217152112+OrkByte@users.noreply.github.com>
2025-08-15 21:36:23 +0700

    Solve Exercises
    Solve exercise 12 to 16.

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
+const palindromes = function (inputStr) {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz1234567890";
 
+  const cleanStrArr = inputStr
+    .toLowerCase()
+    .split("")
+    .filter(letter => alphabet.includes(letter));
+                          
+  let lastIndex = cleanStrArr.length - 1;
+
+  for (let i = 0; i < cleanStrArr.length; i++) {    
+    if (cleanStrArr[i] !== cleanStrArr[lastIndex]) {
+      return false;
+    }
+
+    lastIndex--;
+  }                        
+
+  return true;
 };
 
 // Do not edit below this line
--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+  let penultimate = 0;
+  let previous = 0;
+  let current = 1;
+  let num = Number(number);
 
+  if (num == 0) return 0; 
+  if (num < 1) return "OOPS"; 
+
+  for (let i = 1; i <= num; i++) {
+    current = (penultimate + previous) === 0 ? current : (penultimate + previous);
+    penultimate = previous;
+    previous = current;
+  }
+
+  return current;
 };
+
+// fibonacci(4);
 
 // Do not edit below this line
 module.exports = fibonacci;
+// returns the 4th member of the series: 3  (1, 1, 2, 3)
